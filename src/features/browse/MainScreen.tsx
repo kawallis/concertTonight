a7ffdbf91e558d502a2b75851d969c49e1737898
chris <chris@getmatch.co>
2019-04-13 16:27:50 -0700

    added toggles

--- src/features/browse/MainScreen.tsx
+++ src/features/browse/MainScreen.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Dimensions, AsyncStorage, ActivityIndicator } from 'react-native';
-import { Button, color, Icon, Line } from '../../shared'
+import { StyleSheet, View, Text as RNText, Dimensions, AsyncStorage, ActivityIndicator } from 'react-native';
+import { Button, color, Icon, Line, Text, typography, size } from '../../shared'
 import { FlatGrid } from 'react-native-super-grid';
 import { MapContainer } from './MapContainer';
 import { Ionicons } from '@expo/vector-icons';
@@ -80,9 +80,9 @@ export class MainScreen extends Component<Props, State> {
 		if (viewStyle !== this.state.viewStyle)
 			this.setState({ viewStyle })
 	}
-	toggleListPref = listPref => {
-		if (listPref !== this.state.listPref)
-			this.setState({ listPref })
+	toggleListPref = () => {
+		
+			this.setState({ listPref:this.state.listPref == 'all' ? 'curated' : 'all' })
 	}
 
 	filterEvents = () => {
@@ -123,8 +123,8 @@ export class MainScreen extends Component<Props, State> {
 					const color = getRandomColor();
 					return (
 						<View style={[styles.itemContainer, { backgroundColor: color }]} >
-							<Text style={styles.itemName}>{item.title}</Text>
-							<Text style={styles.venueName}>{item.venue_name}</Text>
+							<RNText style={styles.itemName}>{item.title}</RNText>
+							<RNText style={styles.venueName}>{item.venue_name}</RNText>
 						</View>
 					)
 				}}
@@ -133,7 +133,7 @@ export class MainScreen extends Component<Props, State> {
 	}
 
 	render() {
-		const { events, artists, loading } = this.state
+		const { events, artists, loading,listPref } = this.state
 		if (loading || (!Array.isArray(events) || !events.length) || (!Array.isArray(artists) || !artists.length))
 			return (<View style={styles.container} ><ActivityIndicator size="large" color="#0000ff" /></View>)
 		else {
@@ -149,18 +149,61 @@ export class MainScreen extends Component<Props, State> {
 								justifyContent: "center",
 								flexDirection: "row"
 							}}>
-								<Ionicons name="md-grid" size={20} color="black" onPress={() => this.toggleViewStyle('grid')} />
-								<Text style={{ marginLeft: 10, marginRight: 10 }}>||</Text>
-								<Ionicons name="md-list" size={20} color="black" onPress={() => this.toggleViewStyle('list')} />
 							</View>
 							<View style={{
+								width: '100%',
 								alignItems: 'center',
-								justifyContent: "center",
+								justifyContent: "space-around",
 								flexDirection: "row"
 							}}>
-								<Ionicons name="ios-barcode" size={20} color="black" onPress={() => this.toggleListPref('all')} />
-								<Text style={{ marginLeft: 10, marginRight: 10 }}>||</Text>
-								<Ionicons name="ios-star" size={20} color="yellow" onPress={() => this.toggleListPref('selected')} />
+								<View style={{
+									alignItems: 'center',
+									justifyContent: "space-around",
+									flexDirection: "row"
+								}}>
+									<Ionicons name="md-grid" size={20} color="black" onPress={() => this.toggleViewStyle('grid')} />
+									<Text
+										text='common.grid'
+										style={{
+											...typography.header_1,
+											fontSize: size.large,
+											alignSelf: 'center'
+										}}
+										onPress={() => this.toggleViewStyle('grid')}
+									/>
+								</View>
+								<View style={{
+									alignItems: 'center',
+									justifyContent: "space-around",
+									flexDirection: "row"
+								}}>
+									<Ionicons name="md-list" size={20} color="black" onPress={() => this.toggleViewStyle('list')} />
+									<Text
+										text='common.list'
+										style={{
+											...typography.header_1,
+											fontSize: size.large,
+											alignSelf: 'center'
+										}}
+										onPress={() => this.toggleViewStyle('list')}
+									/>
+								</View>
+								<View style={{
+									alignItems: 'center',
+									justifyContent: "space-around",
+									flexDirection: "row"
+								}}>
+									<Ionicons name="md-grid" size={20} color="black" onPress={() => this.toggleViewStyle('curated')} />
+									<Text
+										text={listPref === 'all' ? 'common.curated' : 'common.all'}
+										style={{
+											...typography.header_1,
+											fontSize: size.large,
+											alignSelf: 'center'
+										}}
+										onPress={() => this.toggleListPref()}
+									/>
+								</View>
 
 							</View>
 						</View>
